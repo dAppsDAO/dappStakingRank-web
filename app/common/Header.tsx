7bc70f1d8c8c0da3319186f6d15abbf0e88745a7
Shintaro Nakagawa <sintaro0221@ShintaronoMacBook-Pro.local>
2022-05-04 22:52:27 +0900

    fix: app name on header

--- app/common/Header.tsx
+++ app/common/Header.tsx
@@ -1,27 +1,27 @@
 import { Link } from "@remix-run/react";
 import type { FC } from "react";
-import { ConnectWallet } from "./ConnectWallet";
-import { SelectNetWork } from "./SelectNetwork";
+// import { ConnectWallet } from "./ConnectWallet";
+// import { SelectNetWork } from "./SelectNetwork";
 
 export const Header: FC = () => {
   return (
     <>
       <div className="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
-        <div className="flex-none">
-          <Link to="/">
+        <Link to="/">
+          <div className="flex-none">
             <button className="btn btn-square btn-ghost">
               <span className="text-4xl">🔥</span>
             </button>
-          </Link>
-        </div>
-        <div className="flex-1 ">
-          <span className="text-lg font-bold">OnFire</span>
-        </div>
-        <div className="flex-none ">
-          {/* <ConnectWallet />
+          </div>
+          <div className="flex-1 ">
+            <span className="text-lg font-bold">Astar Rewards</span>
+          </div>
+        </Link>
+        {/* <div className="flex-none "> */}
+        {/* <ConnectWallet />
           <SelectNetWork /> */}
-          {/* <WalletMultiButton className="btn btn-ghost" /> */}
-        </div>
+        {/* <WalletMultiButton className="btn btn-ghost" /> */}
+        {/* </div> */}
       </div>
     </>
   );
